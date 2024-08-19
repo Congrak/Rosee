c508034a8b3d384c9c643dbeaa6f3cd762eaa938
Rodrigo Celis <105905676+Congrak@users.noreply.github.com>
2024-08-19 12:30:43 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Navbar } from "@/components/navbar";
