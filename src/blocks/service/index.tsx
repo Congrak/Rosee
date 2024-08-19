cce3ce57d57fb9076b84db091827d10b58bcb0fe
Rodrigo Celis <105905676+Congrak@users.noreply.github.com>
2024-08-19 14:21:02 -0400

    Update index.tsx

--- src/blocks/service/index.tsx
+++ src/blocks/service/index.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./service.module.css";
 import { priceList } from "@/contants";
-import { CheckMarkCircleIcon } from "icondex";
+// import { CheckMarkCircleIcon } from "icondex";
 
 export const Service = () => {
   return (
@@ -14,7 +14,7 @@ export const Service = () => {
           {priceList.map((item) => (
             <li key={item.name} className={styles.li}>
               <p className={styles.priceText}>
-              <CheckMarkCircleIcon color="#bb5e3b" height={25} width={25} />
+{/*               <CheckMarkCircleIcon color="#bb5e3b" height={25} width={25} /> */}
                 {item.name}
               </p>
               <p>{item.price}</p>
