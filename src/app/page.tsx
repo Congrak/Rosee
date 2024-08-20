0cb2bdab5591e4a5d2bb546d3f3dd3ef7a866743
Rodrigo Celis <105905676+Congrak@users.noreply.github.com>
2024-08-20 16:57:15 +0000

    creating scroll animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,15 @@ import { Welcome } from "@/blocks/welcome";
 import { Service } from "@/blocks/service";
 import { Carousel } from "@/components/carousel";
 import { Contact } from "@/blocks/contact";
+import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <>
+    <main className={styles.container}>
       <Welcome />
       <Service />
       <Carousel />
       <Contact />
-    </>
+    </main>
   );
 }
