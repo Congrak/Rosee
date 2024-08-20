b51311c9aeb83afa37776a49fb47072cb77e96dd
Rodrigo Celis <105905676+Congrak@users.noreply.github.com>
2024-08-20 16:08:01 +0000

    fix document error

--- src/components/calendly/index.tsx
+++ src/components/calendly/index.tsx
@@ -1,18 +1,26 @@
 "use client";
 
 import { CalandlyProps } from "@/globalTypes"
-import { useRef } from "react"
+import { useEffect, useState } from "react"
 import { PopupModal } from "react-calendly"
 
 export const Calendly = ({ isOpen, handleClick }: CalandlyProps) => {
-    const root = useRef(document.getElementById('root'))
+    const [rootElement, setRootElement] = useState<HTMLElement | null>(null);
+
+    useEffect(() => {
+        if (typeof document !== 'undefined') {
+            const root = document.getElementById('root')
+            setRootElement(root)
+        }
+    }, [])
+
     return (
         <>
             <PopupModal
                 url={process.env.NEXT_PUBLIC_CALENDLY_URL as string}
                 onModalClose={handleClick}
                 open={isOpen}
-                rootElement={root as unknown as HTMLElement}
+                rootElement={rootElement as HTMLElement}
             />
         </>
     )
