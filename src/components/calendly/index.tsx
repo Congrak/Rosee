52afa0f233b793891a0a729401750aa6836d5a80
Rodrigo Celis <105905676+Congrak@users.noreply.github.com>
2024-08-20 15:53:22 +0000

    fixing types for calendly componets

--- src/components/calendly/index.tsx
+++ src/components/calendly/index.tsx
@@ -1,14 +1,18 @@
+"use client";
+
 import { CalandlyProps } from "@/globalTypes"
+import { useRef } from "react"
 import { PopupModal } from "react-calendly"
 
 export const Calendly = ({ isOpen, handleClick }: CalandlyProps) => {
+    const root = useRef(document.getElementById('root'))
     return (
         <>
             <PopupModal
                 url={process.env.NEXT_PUBLIC_CALENDLY_URL as string}
                 onModalClose={handleClick}
                 open={isOpen}
-                rootElement={document.getElementById('root') as HTMLElement}
+                rootElement={root as unknown as HTMLElement}
             />
         </>
     )
