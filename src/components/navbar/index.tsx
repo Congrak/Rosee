dc2a1cf970eedefde7f1b568c70c4422da189754
Rodrigo Celis <105905676+Congrak@users.noreply.github.com>
2024-08-20 21:16:20 +0000

    start fixing size on the section for different screens

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -6,44 +6,44 @@ import { AppointmentButton } from "../appointmentButton";
 export const Navbar = () => {
 
   return (
-    <>
-      <div className={styles.navbar}>
-        <div className={styles.contactBar}>
-          <div className={styles.contactInfo}>
-            <div>email</div>
-            <div>wasa</div>
-          </div>
-          <div className={styles.contactInfo}>
-            <p>Siguenos</p>
-            <div>instagram</div>
-            <div>facebook</div>
-            <div>tiktok</div>
-          </div>
+
+    <div className={styles.navbar}>
+      <div className={styles.contactBar}>
+        <div className={styles.contactInfo}>
+          <div>email</div>
+          <div>wasa</div>
         </div>
-        <nav className={styles.container}>
-          <div className={styles.logo}>
-            <Image
-              src="/logo.png"
-              alt="logo"
-              width={200}
-              height={100}
-              layout="cover"
-            />
-          </div>
-          <div>
-            <ul className={styles.ul}>
-              {menuList.map((item) => (
-                <li key={item} className={styles.li}>
-                  {item}
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className={styles.buttonContainer} id="root">
-            <AppointmentButton />
-          </div>
-        </nav>
+        <div className={styles.contactInfo}>
+          <p>Siguenos</p>
+          <div>instagram</div>
+          <div>facebook</div>
+          <div>tiktok</div>
+        </div>
       </div>
-    </>
+      <nav className={styles.container}>
+        <div className={styles.logo}>
+          <Image
+            src="/logo.png"
+            alt="logo"
+            width={200}
+            height={100}
+            layout="cover"
+          />
+        </div>
+        <div>
+          <ul className={styles.ul}>
+            {menuList.map((item) => (
+              <li key={item} className={styles.li}>
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className={styles.buttonContainer} id="root">
+          <AppointmentButton />
+        </div>
+      </nav>
+    </div>
+
   );
 };
